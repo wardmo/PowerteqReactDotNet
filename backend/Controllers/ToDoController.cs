aab5e5981adf75484425fc838d14c4d843a76ab8
mward-ptq <80487298+mward-ptq@users.noreply.github.com>
2021-03-31 20:38:35 -0600

    scaffolding and start point

---
+++ backend/Controllers/ToDoController.cs
@@ -0,0 +1,27 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace backend.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ToDoController : ControllerBase
+    {
+        private readonly ILogger<ToDoController> _logger;
+
+        public ToDoController(ILogger<ToDoController> logger)
+        {
+            _logger = logger;
+        }
+
+        [HttpGet]
+        public string Get()
+        {
+            return "Hello World";
+        }
+    }
+}
